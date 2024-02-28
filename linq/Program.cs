8dd20b36f2fa76b1173d1203ef284f7b0917bf53
John Doe <navrotskyi.maksym@lll.kpi.ua>
2024-02-28 11:24:26 +0200

    first commit

---
+++ linq/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        
+        List<int> list = new List<int> { 3, 8, 12, 6, 9, 18, 5, 14 };
+
+        double average = list.Where(t => t >= 5 && t <= 15).Average();
+
+        Console.WriteLine($"Середнє арифметичне елементів у діапазоні [5, 15]: {average}");
+    }
+}
